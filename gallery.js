b48dc1d17f86e5d2dbb831aa29f9df98b2875273
Clover <71147866+EmpressClover@users.noreply.github.com>
2023-05-04 10:34:22 -0700

    mAIN sITE

---
+++ gallery.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+    $('.image').on('click', function(){
+    $(this).toggleClass('image2');
+  });
+  });
+
+
+  $(document).ready(function(){
+    $('.image4').on('click', function(){
+    $(this).toggleClass('image2');
+  });
+  });
+
+
+  $(document).ready(function(){
+    $('.image5').on('click', function(){
+    $(this).toggleClass('image2');
+  });
+  });
